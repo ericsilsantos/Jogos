87ca7081c70efe3d745df21308f506027e292b62
Eric Santos <eric.silvasantos44@gmail.com>
2022-04-04 21:22:28 -0300

    Adicionado a lógica para mostrar que alguem ganhou

--- Jogo da Velha/script.js
+++ Jogo da Velha/script.js
@@ -1,7 +1,49 @@
 let colocarX = true;
+let xisSelecionados = [];
+let bolaSelecionados = [];
+const posicaoVencedoras = [
+  ["0","1","2"],
+  ["3","4","5"],
+  ["6","7","8"],
+  ["0","3","6"],
+  ["1","4","7"],
+  ["2","5","8"],
+  ["0","4","8"],
+  ["6","4","2"],
+];
+
+const verificar = (data, simbolo) => {
+  const ganhou = posicaoVencedoras.some((array) =>
+    array.every((item) => 
+      data.includes(item)
+    )
+  )
+  if (ganhou) {
+    const body = document.querySelector("#body")
+    const textoGanhador = document.createElement('p')
+    textoGanhador.classList.add("textoGanhador")
+    textoGanhador.innerText = `Jogador ${simbolo} Ganhou!!!`
+    body.appendChild(textoGanhador);
+  }
+}
+
+const testeSeGanhou = (simbolo, id) => {
+  switch(simbolo) {
+    case "xis":
+      xisSelecionados.push(id)
+      verificar(xisSelecionados, "X")
+      break;
+    case "bola":
+      bolaSelecionados.push(id)
+      verificar(bolaSelecionados, "O")
+      break;
+    default:
+      console.log("ERRO!!!")
+      break;
+    }
+}
 
 const colocarSimbolo = (casa) => {
-  // const casa = document.querySelector(id) 
   if (!casa.classList.contains("select")) {
     if(colocarX) {
       const xis = document.createElement('p')
@@ -9,15 +51,16 @@ const colocarSimbolo = (casa) => {
       xis.innerText = "X"
       casa.appendChild(xis)
       casa.classList.add("select")
+      testeSeGanhou('xis', casa.id)
     } else {
       const bola = document.createElement('p')
       bola.classList.add("simbolo")
       bola.innerText = "O"
       casa.appendChild(bola)
       casa.classList.add("select")
+      testeSeGanhou('bola', casa.id)
     }
     colocarX = !colocarX
-    console.log(colocarX)
   }
 }
 
